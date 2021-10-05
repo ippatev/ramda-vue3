42368df0563545b4e1f62184515d50afeafebc5f
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2021-10-05 21:30:46 +0900

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+const R = require("ramda");
+
+export default {
+  install: (app, options) => {
+    app.config.globalProperties.$R: {
+      get() {
+        return R
+      }
+    }
+
+    R.ifElse(
+      R.and(R.compose(R.not, R.isNil), R.has("Vue")),
+      (win) => {
+        win.Vue.use(VueRamda);
+      },
+      () => {}
+    )(window);
+  }
+}
